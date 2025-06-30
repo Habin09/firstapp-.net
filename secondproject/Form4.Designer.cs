0c0121c395d9b6e018e8277a1a43276abd3968be
Habin09 <habihabinan0@gmail.com>
2025-06-30 22:09:04 +0100

    Student Registration

---
+++ secondproject/Form4.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace Secondproject
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lbl6 = new Label();
+            rdofemale = new RadioButton();
+            rdomale = new RadioButton();
+            txtlname = new TextBox();
+            lbl5 = new Label();
+            lbl4 = new Label();
+            txtname = new TextBox();
+            lbl3 = new Label();
+            txtadmission = new TextBox();
+            lbl2 = new Label();
+            lbl = new Label();
+            nudgrade = new NumericUpDown();
+            lbl7 = new Label();
+            chkmaths = new CheckBox();
+            chktamil = new CheckBox();
+            chkenglish = new CheckBox();
+            lbl8 = new Label();
+            textBox4 = new TextBox();
+            btn1 = new Button();
+            button2 = new Button();
+            ((System.ComponentModel.ISupportInitialize)nudgrade).BeginInit();
+            SuspendLayout();
+            // 
+            // lbl6
+            // 
+            lbl6.AutoSize = true;
+            lbl6.Font = new Font("Microsoft JhengHei", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lbl6.Location = new Point(21, 259);
+            lbl6.Name = "lbl6";
+            lbl6.Size = new Size(61, 22);
+            lbl6.TabIndex = 21;
+            lbl6.Text = "Grade";
+            // 
+            // rdofemale
+            // 
+            rdofemale.AutoSize = true;
+            rdofemale.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            rdofemale.Location = new Point(262, 212);
+            rdofemale.Name = "rdofemale";
+            rdofemale.Size = new Size(80, 24);
+            rdofemale.TabIndex = 20;
+            rdofemale.TabStop = true;
+            rdofemale.Text = "Female";
+            rdofemale.UseVisualStyleBackColor = true;
+            // 
+            // rdomale
+            // 
+            rdomale.AutoSize = true;
+            rdomale.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            rdomale.Location = new Point(171, 212);
+            rdomale.Name = "rdomale";
+            rdomale.Size = new Size(64, 24);
+            rdomale.TabIndex = 19;
+            rdomale.TabStop = true;
+            rdomale.Text = "Male";
+            rdomale.UseVisualStyleBackColor = true;
+            // 
+            // txtlname
+            // 
+            txtlname.Location = new Point(157, 167);
+            txtlname.Name = "txtlname";
+            txtlname.Size = new Size(343, 27);
+            txtlname.TabIndex = 18;
+            // 
+            // lbl5
+            // 
+            lbl5.AutoSize = true;
+            lbl5.Font = new Font("Microsoft JhengHei", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lbl5.Location = new Point(21, 214);
+            lbl5.Name = "lbl5";
+            lbl5.Size = new Size(72, 22);
+            lbl5.TabIndex = 17;
+            lbl5.Text = "Gender";
+            // 
+            // lbl4
+            // 
+            lbl4.AutoSize = true;
+            lbl4.Font = new Font("Microsoft JhengHei", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lbl4.Location = new Point(21, 172);
+            lbl4.Name = "lbl4";
+            lbl4.Size = new Size(95, 22);
+            lbl4.TabIndex = 16;
+            lbl4.Text = "Last name";
+            // 
+            // txtname
+            // 
+            txtname.Location = new Point(157, 124);
+            txtname.Name = "txtname";
+            txtname.Size = new Size(343, 27);
+            txtname.TabIndex = 15;
+            // 
+            // lbl3
+            // 
+            lbl3.AutoSize = true;
+            lbl3.FlatStyle = FlatStyle.Flat;
+            lbl3.Font = new Font("Microsoft JhengHei", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lbl3.Location = new Point(21, 129);
+            lbl3.Name = "lbl3";
+            lbl3.Size = new Size(97, 22);
+            lbl3.TabIndex = 14;
+            lbl3.Text = "First name";
+            // 
+            // txtadmission
+            // 
+            txtadmission.Location = new Point(157, 79);
+            txtadmission.Name = "txtadmission";
+            txtadmission.Size = new Size(343, 27);
+            txtadmission.TabIndex = 13;
+            // 
+            // lbl2
+            // 
+            lbl2.AutoSize = true;
+            lbl2.Font = new Font("Microsoft JhengHei", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lbl2.Location = new Point(21, 84);
+            lbl2.Name = "lbl2";
+            lbl2.Size = new Size(130, 22);
+            lbl2.TabIndex = 12;
+            lbl2.Text = "Admisstion.no";
+            // 
+            // lbl
+            // 
+            lbl.AutoSize = true;
+            lbl.Font = new Font("Microsoft PhagsPa", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lbl.Location = new Point(189, 9);
+            lbl.Name = "lbl";
+            lbl.Size = new Size(237, 31);
+            lbl.TabIndex = 11;
+            lbl.Text = "Student Registration";
+            // 
+            // nudgrade
+            // 
+            nudgrade.Location = new Point(157, 254);
+            nudgrade.Name = "nudgrade";
+            nudgrade.Size = new Size(125, 27);
+            nudgrade.TabIndex = 22;
+            // 
+            // lbl7
+            // 
+            lbl7.AutoSize = true;
+            lbl7.Font = new Font("Microsoft JhengHei", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lbl7.Location = new Point(21, 313);
+            lbl7.Name = "lbl7";
+            lbl7.Size = new Size(73, 22);
+            lbl7.TabIndex = 23;
+            lbl7.Text = "Subject";
+            // 
+            // chkmaths
+            // 
+            chkmaths.AutoSize = true;
+            chkmaths.Font = new Font("Segoe UI Black", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            chkmaths.Location = new Point(157, 313);
+            chkmaths.Name = "chkmaths";
+            chkmaths.Size = new Size(76, 24);
+            chkmaths.TabIndex = 24;
+            chkmaths.Text = "Maths";
+            chkmaths.UseVisualStyleBackColor = true;
+            // 
+            // chktamil
+            // 
+            chktamil.AutoSize = true;
+            chktamil.Font = new Font("Segoe UI Black", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            chktamil.Location = new Point(157, 343);
+            chktamil.Name = "chktamil";
+            chktamil.Size = new Size(73, 24);
+            chktamil.TabIndex = 25;
+            chktamil.Text = "Tamil";
+            chktamil.UseVisualStyleBackColor = true;
+            // 
+            // chkenglish
+            // 
+            chkenglish.AutoSize = true;
+            chkenglish.Font = new Font("Segoe UI Black", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            chkenglish.Location = new Point(157, 373);
+            chkenglish.Name = "chkenglish";
+            chkenglish.Size = new Size(84, 24);
+            chkenglish.TabIndex = 26;
+            chkenglish.Text = "English";
+            chkenglish.UseVisualStyleBackColor = true;
+            // 
+            // lbl8
+            // 
+            lbl8.AutoSize = true;
+            lbl8.Font = new Font("Microsoft JhengHei", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lbl8.Location = new Point(21, 411);
+            lbl8.Name = "lbl8";
+            lbl8.Size = new Size(69, 19);
+            lbl8.TabIndex = 27;
+            lbl8.Text = "Address";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(157, 407);
+            textBox4.Multiline = true;
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(300, 26);
+            textBox4.TabIndex = 28;
+            // 
+            // btn1
+            // 
+            btn1.Font = new Font("Segoe UI Emoji", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn1.Location = new Point(103, 449);
+            btn1.Name = "btn1";
+            btn1.Size = new Size(132, 41);
+            btn1.TabIndex = 29;
+            btn1.Text = "Save";
+            btn1.UseVisualStyleBackColor = true;
+            btn1.Click += btn1_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI Historic", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.Location = new Point(324, 450);
+            button2.Name = "button2";
+            button2.Size = new Size(133, 41);
+            button2.TabIndex = 30;
+            button2.Text = "Cancel";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // Form4
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(535, 502);
+            Controls.Add(button2);
+            Controls.Add(btn1);
+            Controls.Add(textBox4);
+            Controls.Add(lbl8);
+            Controls.Add(chkenglish);
+            Controls.Add(chktamil);
+            Controls.Add(chkmaths);
+            Controls.Add(lbl7);
+            Controls.Add(nudgrade);
+            Controls.Add(lbl6);
+            Controls.Add(rdofemale);
+            Controls.Add(rdomale);
+            Controls.Add(txtlname);
+            Controls.Add(lbl5);
+            Controls.Add(lbl4);
+            Controls.Add(txtname);
+            Controls.Add(lbl3);
+            Controls.Add(txtadmission);
+            Controls.Add(lbl2);
+            Controls.Add(lbl);
+            Name = "Form4";
+            Text = "Form4";
+            ((System.ComponentModel.ISupportInitialize)nudgrade).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lbl6;
+        private RadioButton rdofemale;
+        private RadioButton rdomale;
+        private TextBox txtlname;
+        private Label lbl5;
+        private Label lbl4;
+        private TextBox txtname;
+        private Label lbl3;
+        private TextBox txtadmission;
+        private Label lbl2;
+        private Label lbl;
+        private NumericUpDown nudgrade;
+        private Label lbl7;
+        private CheckBox chkmaths;
+        private CheckBox chktamil;
+        private CheckBox chkenglish;
+        private Label lbl8;
+        private TextBox textBox4;
+        private Button btn1;
+        private Button button2;
+    }
+}
---
+++ secondproject/Form4.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Secondproject
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            string gender = rdomale.Checked ? rdomale.Text : rdofemale.Text;
+            List<string> selectedSubjects = new List<string>();
+            if (chkenglish.Checked) selectedSubjects.Add(chkenglish.Text);
+            if (chkmaths.Checked) selectedSubjects.Add(chkmaths.Text);
+            if (chktamil.Checked) selectedSubjects.Add(chktamil.Text);
+            string subjects = string.Join(", ", selectedSubjects);
+            MessageBox.Show(
+                $"Admission Number: {txtadmission.Text}\n" +
+                $"First Name: {txtname.Text}\n" +
+                $"Last Name: {txtlname.Text}\n" +
+                $"Subjects: {subjects}\n" +
+                $"Grade :{nudgrade.Text}\n" +
+                $"Gender: {gender}",
+                "Student Details",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information
+            );
+        }
+    }
+}
