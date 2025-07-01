50b41bc199e5e547b13360afcafaefecb4f245ee
Habin09 <habihabinan0@gmail.com>
2025-07-01 09:25:06 +0100

    Employ Job Application

--- secondproject/Form1.Designer.cs
+++ secondproject/Form1.Designer.cs
@@ -3,12 +3,12 @@
     partial class Form1
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -23,105 +23,169 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
             lbl = new Label();
-            lbl2 = new Label();
-            textBox1 = new TextBox();
-            lbl3 = new Label();
-            textBox2 = new TextBox();
-            lbl4 = new Label();
-            lbl5 = new Label();
-            textBox3 = new TextBox();
+            lblpersonal = new Label();
+            lblname = new Label();
+            txtname = new TextBox();
+            lbladdress = new Label();
+            txtaddress = new TextBox();
+            lblphone = new Label();
+            txtphone = new TextBox();
+            lblemail = new Label();
+            txtemail = new TextBox();
+            lblid = new Label();
+            txtid = new TextBox();
+            btnapply = new Button();
+            lblgender = new Label();
             rdomale = new RadioButton();
             rdofemale = new RadioButton();
-            lbl6 = new Label();
+            pnl = new Panel();
+            pnl.SuspendLayout();
             SuspendLayout();
             // 
             // lbl
             // 
             lbl.AutoSize = true;
-            lbl.Font = new Font("Microsoft PhagsPa", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            lbl.Location = new Point(211, 21);
+            lbl.BackColor = Color.Transparent;
+            lbl.Font = new Font("Microsoft New Tai Lue", 16.2F, FontStyle.Bold | FontStyle.Underline, GraphicsUnit.Point, 0);
+            lbl.ForeColor = SystemColors.ButtonHighlight;
+            lbl.Location = new Point(116, 25);
             lbl.Name = "lbl";
-            lbl.Size = new Size(237, 31);
+            lbl.Size = new Size(299, 37);
             lbl.TabIndex = 0;
-            lbl.Text = "Student Registration";
+            lbl.Text = "Job Application Form";
             // 
-            // lbl2
+            // lblpersonal
             // 
-            lbl2.AutoSize = true;
-            lbl2.Font = new Font("Microsoft JhengHei", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            lbl2.Location = new Point(43, 96);
-            lbl2.Name = "lbl2";
-            lbl2.Size = new Size(130, 22);
-            lbl2.TabIndex = 1;
-            lbl2.Text = "Admisstion.no";
+            lblpersonal.AutoSize = true;
+            lblpersonal.Font = new Font("Microsoft New Tai Lue", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblpersonal.Location = new Point(24, 86);
+            lblpersonal.Name = "lblpersonal";
+            lblpersonal.Size = new Size(147, 20);
+            lblpersonal.TabIndex = 1;
+            lblpersonal.Text = "Personal information";
             // 
-            // textBox1
+            // lblname
             // 
-            textBox1.Location = new Point(179, 91);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(343, 27);
-            textBox1.TabIndex = 2;
+            lblname.AutoSize = true;
+            lblname.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblname.Location = new Point(57, 106);
+            lblname.Name = "lblname";
+            lblname.Size = new Size(84, 18);
+            lblname.TabIndex = 2;
+            lblname.Text = "Full Name";
             // 
-            // lbl3
+            // txtname
             // 
-            lbl3.AutoSize = true;
-            lbl3.FlatStyle = FlatStyle.Flat;
-            lbl3.Font = new Font("Microsoft JhengHei", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            lbl3.Location = new Point(43, 141);
-            lbl3.Name = "lbl3";
-            lbl3.Size = new Size(97, 22);
-            lbl3.TabIndex = 3;
-            lbl3.Text = "First name";
+            txtname.Location = new Point(62, 127);
+            txtname.Name = "txtname";
+            txtname.Size = new Size(383, 27);
+            txtname.TabIndex = 3;
             // 
-            // textBox2
+            // lbladdress
             // 
-            textBox2.Location = new Point(179, 136);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(343, 27);
-            textBox2.TabIndex = 4;
+            lbladdress.AutoSize = true;
+            lbladdress.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lbladdress.Location = new Point(57, 264);
+            lbladdress.Name = "lbladdress";
+            lbladdress.Size = new Size(69, 18);
+            lbladdress.TabIndex = 4;
+            lbladdress.Text = "Address";
             // 
-            // lbl4
+            // txtaddress
             // 
-            lbl4.AutoSize = true;
-            lbl4.Font = new Font("Microsoft JhengHei", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            lbl4.Location = new Point(43, 184);
-            lbl4.Name = "lbl4";
-            lbl4.Size = new Size(95, 22);
-            lbl4.TabIndex = 5;
-            lbl4.Text = "Last name";
+            txtaddress.Location = new Point(62, 285);
+            txtaddress.Multiline = true;
+            txtaddress.Name = "txtaddress";
+            txtaddress.Size = new Size(380, 32);
+            txtaddress.TabIndex = 5;
             // 
-            // lbl5
+            // lblphone
             // 
-            lbl5.AutoSize = true;
-            lbl5.Font = new Font("Microsoft JhengHei", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            lbl5.Location = new Point(43, 226);
-            lbl5.Name = "lbl5";
-            lbl5.Size = new Size(72, 22);
-            lbl5.TabIndex = 6;
-            lbl5.Text = "Gender";
-            lbl5.Click += label2_Click;
+            lblphone.AutoSize = true;
+            lblphone.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblphone.Location = new Point(57, 335);
+            lblphone.Name = "lblphone";
+            lblphone.Size = new Size(120, 18);
+            lblphone.TabIndex = 6;
+            lblphone.Text = "Phone Number";
             // 
-            // textBox3
+            // txtphone
             // 
-            textBox3.Location = new Point(179, 179);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(343, 27);
-            textBox3.TabIndex = 7;
+            txtphone.Location = new Point(62, 356);
+            txtphone.Name = "txtphone";
+            txtphone.Size = new Size(246, 27);
+            txtphone.TabIndex = 7;
+            // 
+            // lblemail
+            // 
+            lblemail.AutoSize = true;
+            lblemail.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblemail.Location = new Point(57, 402);
+            lblemail.Name = "lblemail";
+            lblemail.Size = new Size(116, 18);
+            lblemail.TabIndex = 8;
+            lblemail.Text = "Email Address";
+            // 
+            // txtemail
+            // 
+            txtemail.Location = new Point(62, 423);
+            txtemail.Name = "txtemail";
+            txtemail.Size = new Size(389, 27);
+            txtemail.TabIndex = 9;
+            // 
+            // lblid
+            // 
+            lblid.AutoSize = true;
+            lblid.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblid.Location = new Point(57, 467);
+            lblid.Name = "lblid";
+            lblid.Size = new Size(88, 18);
+            lblid.TabIndex = 10;
+            lblid.Text = "ID Number";
+            // 
+            // txtid
+            // 
+            txtid.Location = new Point(62, 488);
+            txtid.Name = "txtid";
+            txtid.Size = new Size(327, 27);
+            txtid.TabIndex = 11;
+            // 
+            // btnapply
+            // 
+            btnapply.BackColor = SystemColors.MenuHighlight;
+            btnapply.Font = new Font("Segoe UI Symbol", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnapply.Location = new Point(177, 552);
+            btnapply.Name = "btnapply";
+            btnapply.Size = new Size(189, 40);
+            btnapply.TabIndex = 14;
+            btnapply.Text = "Apply Now";
+            btnapply.UseVisualStyleBackColor = false;
+            btnapply.Click += btnapply_Click;
+            // 
+            // lblgender
+            // 
+            lblgender.AutoSize = true;
+            lblgender.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblgender.Location = new Point(57, 169);
+            lblgender.Name = "lblgender";
+            lblgender.Size = new Size(63, 18);
+            lblgender.TabIndex = 15;
+            lblgender.Text = "Gender";
             // 
             // rdomale
             // 
             rdomale.AutoSize = true;
-            rdomale.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
-            rdomale.Location = new Point(193, 224);
+            rdomale.Font = new Font("Segoe UI Symbol", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            rdomale.Location = new Point(77, 190);
             rdomale.Name = "rdomale";
-            rdomale.Size = new Size(64, 24);
-            rdomale.TabIndex = 8;
+            rdomale.Size = new Size(63, 24);
+            rdomale.TabIndex = 16;
             rdomale.TabStop = true;
             rdomale.Text = "Male";
             rdomale.UseVisualStyleBackColor = true;
@@ -129,44 +193,50 @@
             // rdofemale
             // 
             rdofemale.AutoSize = true;
-            rdofemale.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
-            rdofemale.Location = new Point(284, 224);
+            rdofemale.Font = new Font("Segoe UI Symbol", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            rdofemale.Location = new Point(77, 220);
             rdofemale.Name = "rdofemale";
-            rdofemale.Size = new Size(80, 24);
-            rdofemale.TabIndex = 9;
+            rdofemale.Size = new Size(78, 24);
+            rdofemale.TabIndex = 17;
             rdofemale.TabStop = true;
             rdofemale.Text = "Female";
             rdofemale.UseVisualStyleBackColor = true;
             // 
-            // lbl6
+            // pnl
             // 
-            lbl6.AutoSize = true;
-            lbl6.Font = new Font("Microsoft JhengHei", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            lbl6.Location = new Point(43, 271);
-            lbl6.Name = "lbl6";
-            lbl6.Size = new Size(61, 22);
-            lbl6.TabIndex = 10;
-            lbl6.Text = "Gread";
-            lbl6.Click += lbl6_Click;
+            pnl.BackColor = Color.LimeGreen;
+            pnl.Controls.Add(lbl);
+            pnl.Location = new Point(0, 1);
+            pnl.Name = "pnl";
+            pnl.Size = new Size(539, 81);
+            pnl.TabIndex = 18;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(698, 450);
-            Controls.Add(lbl6);
+            BackColor = Color.Gainsboro;
+            ClientSize = new Size(541, 654);
+            Controls.Add(pnl);
             Controls.Add(rdofemale);
             Controls.Add(rdomale);
-            Controls.Add(textBox3);
-            Controls.Add(lbl5);
-            Controls.Add(lbl4);
-            Controls.Add(textBox2);
-            Controls.Add(lbl3);
-            Controls.Add(textBox1);
-            Controls.Add(lbl2);
-            Controls.Add(lbl);
+            Controls.Add(lblgender);
+            Controls.Add(btnapply);
+            Controls.Add(txtid);
+            Controls.Add(lblid);
+            Controls.Add(txtemail);
+            Controls.Add(lblemail);
+            Controls.Add(txtphone);
+            Controls.Add(lblphone);
+            Controls.Add(txtaddress);
+            Controls.Add(lbladdress);
+            Controls.Add(txtname);
+            Controls.Add(lblname);
+            Controls.Add(lblpersonal);
             Name = "Form1";
             Text = "Form1";
+            pnl.ResumeLayout(false);
+            pnl.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -174,15 +244,21 @@
         #endregion
 
         private Label lbl;
-        private Label lbl2;
-        private TextBox textBox1;
-        private Label lbl3;
-        private TextBox textBox2;
-        private Label lbl4;
-        private Label lbl5;
-        private TextBox textBox3;
+        private Label lblpersonal;
+        private Label lblname;
+        private TextBox txtname;
+        private Label lbladdress;
+        private TextBox txtaddress;
+        private Label lblphone;
+        private TextBox txtphone;
+        private Label lblemail;
+        private TextBox txtemail;
+        private Label lblid;
+        private TextBox txtid;
+        private Button btnapply;
+        private Label lblgender;
         private RadioButton rdomale;
         private RadioButton rdofemale;
-        private Label lbl6;
+        private Panel pnl;
     }
 }
--- secondproject/Form1.cs
+++ secondproject/Form1.cs
@@ -1,71 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
 namespace Secondproject
 {
-    public partial class Form1
-        : Form
+    public partial class Form1 : Form
     {
-        public Form1()    
+        public Form1()
         {
             InitializeComponent();
         }
 
-        private void label2_Click(object sender, EventArgs e)
+        private void btnapply_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void lbl6_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void checkBox1_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void nud_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-           // MessageBox.Show(txt4.Text);
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-           // MessageBox.Show(.Te);
-        }
-
-        private void textBox3_TextChanged(object sender, EventArgs e)
-        {
-           // MessageBox.Show(txt4.Text);
-        }
-
-        private void txt4_TextChanged(object sender, EventArgs e)
-        {
-           // MessageBox.Show(txt4.Text);
-        }
-
-        private void rdomale_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void btn1_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("Correct Details", "information", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-        }
-
-        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
-        {
-
+            String gender = rdomale.Checked ? rdomale.Text : rdofemale.Text;
+            MessageBox.Show(
+                $"Full Name :{txtname.Text}\n" +
+                $"Gender :{gender}\n" +
+                $"Address :{txtaddress.Text}\n" +
+                $"Phone Number :{txtphone.Text}\n" +
+                $"Email Address :{txtemail.Text}\n" +
+                $"Id Number :{txtid.Text}", 
+                "Employ Details",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Question
+            );
         }
     }
 }
